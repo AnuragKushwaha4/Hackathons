f94e7fcec2868a0f8c043085a603c1fc615a8f35
AnuragKushwaha4 <anuragjnv4@gmail.com>
2025-12-10 22:22:27 +0530

    problem with js

---
+++ React/introduction.js
@@ -0,0 +1,7 @@
+const h1 = document.createElement("h1");
+h1.textContent = "Hello Guy Welcome to the Coder world";
+
+h1.className ="element";
+
+const root = document.getElementById("root");
+root.append(h1);
