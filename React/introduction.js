1da77f4f65187c71f020efdbade64857248c97e6
AnuragKushwaha4 <anuragjnv4@gmail.com>
2025-12-10 23:35:07 +0530

    Custom React

--- React/introduction.js
+++ React/introduction.js
@@ -5,3 +5,36 @@ h1.className ="element";
 
 const root = document.getElementById("root");
 root.append(h1);
+
+
+const React = {
+    createElement : function(type, props,children){
+        return {
+            type :type,
+            props:{...props,
+                children:children
+            }
+        }
+    }
+}
+
+
+const ReactDOM = {
+    render: function(ReactElement, root){
+        const element = document.createElement(ReactElement.type);
+        const props = ReactElement.props;
+
+        for(const key in props){
+            if(key==="style"){
+                Object.assign(element.style,props.style);
+            }
+            else if(key ==="children"){
+                element.textContent = props[key];
+            }
+            else{element[key]=props[key];}
+
+
+        }
+        root.append(eleemnt);
+    }
+}
