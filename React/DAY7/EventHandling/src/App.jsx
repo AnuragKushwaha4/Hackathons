e8bda3251b2a185d512242e6382c3c53cc543949
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-02-16 22:02:56 +0530

    completed with event handling

--- React/DAY7/EventHandling/src/App.jsx
+++ React/DAY7/EventHandling/src/App.jsx
@@ -4,30 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [val,setValue]=useState("")
+  const handlechange=(e)=>{
+    setValue(e.target.value.toUpperCase())
+  }
 
+  const handlesubmit =(e)=>{
+    e.preventDefault();
+    alert(`The submitted name is: ${val}`)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* first we understand the working in form */}
+    <form onSubmit={handlesubmit}>
+      <input type="text" value={val} onChange={handlechange} />
+      <button type='submit' >Submit</button>
+    </form>
     </>
   )
 }
