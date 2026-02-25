7631854d31d93158afcb5b8b8aed1baa280a8415
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-02-25 18:02:53 +0530

    Completed with useCallback

--- React/DAY15/Use Callback/src/App.jsx
+++ React/DAY15/Use Callback/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Child from './Components/Child'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleIncrement = useCallback(()=>{
+    setCount(count+1)
+  },[count])
   return (
     <>
       <div>{count}</div>
+      <div><button onClick={handleIncrement}>Increment</button></div>
+
+      <Child name="Child Button"  setCount={setCount}/>
     </>
   )
 }
--- React/DAY15/Use Callback/src/Components/Child.jsx
+++ React/DAY15/Use Callback/src/Components/Child.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "../App.css"
 const Child = React.memo(
     (props) => {
+        console.log("Child component renderd");
   return (
     <div>
       <button>{props.name}</button>
