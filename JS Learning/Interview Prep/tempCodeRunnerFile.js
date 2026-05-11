43746f851b050a3d26e1a13644bdccd058628554
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-05-11 19:49:26 +0530

    JS prep started

--- JS Learning/Interview Prep/tempCodeRunnerFile.js
+++ JS Learning/Interview Prep/tempCodeRunnerFile.js
@@ -1,25 +1,37 @@
-let str = "Anurag"
+// let str = "Anurag"
 
-let answer =""
-for(let i =str.length-1;i>=0;i--){
-    answer+=str[i];
+// let answer =""
+// for(let i =str.length-1;i>=0;i--){
+//     answer+=str[i];
+// }
+
+// console.log(answer)
+
+// let arr = [1,3,4,34,54]
+
+
+
+
+// arr.forEach((element)=>{
+//     console.log(element*2)
+// })
+
+// let arr2 = arr.map((element)=>{
+//     return arr/2;
+// })
+// console.log(arr2);
+// let arr3 = arr.filter((element)=>element>10)
+// console.log(arr3);
+
+
+var var1 = 2;
+var var2 = 3;
+{
+    var var1 =3;
+    let var2 = 4;
+    console.log(var2)
+    console.log(var1);
 }
 
-console.log(answer)
-
-let arr = [1,3,4,34,54]
-
-
-
-
-arr.forEach((element)=>{
-    console.log(element*2)
-})
-
-let arr2 = arr.map((element)=>{
-    return arr/2;
-})
-console.log(arr2);
-let arr3 = arr.filter((element)=>element>10)
-console.log(arr3);
-
+console.log(var2)
+console.log(var1);
